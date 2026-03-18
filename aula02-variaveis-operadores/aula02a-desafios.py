ea1a36d243354a5570625deb847fe44373044fc3
muriloignaccio <murilohsignacio@gmail.com>
2026-03-18 20:50:31 -0300

    first commit

---
+++ aula02-variaveis-operadores/aula02a-desafios.py
@@ -0,0 +1,13 @@
+name = input("Qual o seu nome?")
+print(f"Seja bem vindo, {name}")
+
+day = input("Que dia é hoje? ")
+month = input("Que mês é hoje? ")
+year = input("Que ano é hoje?")
+
+print(f"A data de hoje é {day}/{month}/{year}")
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print(num1 + num2)
